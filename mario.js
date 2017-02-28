74d32bee3eab888a874d781d8c72cdd7fdf5c65f
Perigree <holisticspiral@gmail.com>
2017-02-27 19:40:17 -0500

    Update mario.js

--- mario.js
+++ mario.js
@@ -19,5 +19,36 @@ function printPyramid(height) {
 
     // TODO
     // print that pyramid!
+    printPyramid(5);
+
+
+/*
+ * printPyramid
+ *
+ * Prints to the console a pyramid of '#' characters of the specified height
+ * For example, if height is 5, the console will look like this:
+ *          ##
+ *         ###
+ *        ####
+ *       #####
+ *      ######
+ */
+function printPyramid(height) {
+    console.log("Uh oh... the pyramid is under construction.");
+    console.log("Check back soon, our developers are hard at work as we speak!");
+
+    // TODO
+    // print that pyramid!
+    var hash = '';
+
+    for (var counter = 0;counter < 6 ; counter ++)
+    {
+     console.log (hash);
+     hash= hash + "#";
+    }
+    console.log ("Print that pyramid!");
+
+}
+
 
 }
